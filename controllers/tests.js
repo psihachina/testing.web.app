51a90c640383e9a833c7ee3e4c2dad4e161242d7
owlkek <46930203+owlkek@users.noreply.github.com>
2020-05-26 09:21:19 +0300

    Controllers, views
    Добавлена функция отправки тестов.
    Добавлены файл представления для вывода результата (необходимо доделать)

--- controllers/tests.js
+++ controllers/tests.js
@@ -51,3 +51,68 @@ exports.show_test = function(req, res, next) {
         });
     });
 }
+exports.send_test = function(req, res, next) {
+    let idattempt;
+    return models.Question.findAll({
+        where: {
+            idTest: req.params.id
+        }
+    }).then(questions => {
+
+        return models.Attempt.create({
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            idUser: 4,
+            idTest: req.params.id,
+            DateStart: new Date(),
+            DateFinish: new Date()
+        }).then(async attempt => {
+            idattempt = attempt.idAttempt;
+
+            for await (element of questions) {
+                let idElem = element.idQuestion;
+                console.log(req.body[idElem] + " " + typeof(req.body[idElem]));
+                console.log(typeof(req.body[idElem][0]))
+
+                if (req.body[idElem].length > 1)
+                    for await (item of req.body[idElem]) {
+                        models.TestUserAnswer.create({
+                            createdAt: new Date(),
+                            updatedAt: new Date(),
+                            idAttempt: attempt.idAttempt,
+                            idQuestion: idElem,
+                            idAnswer: item
+                        });
+                    }
+                else {
+                    models.TestUserAnswer.create({
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        idAttempt: attempt.idAttempt,
+                        idQuestion: idElem,
+                        idAnswer: req.body[idElem][0]
+                    });
+
+                }
+            };
+        }).then(next => {
+            return models.Answer.findAll({
+                idQuestion: {
+                    [Op.or]: getIdQuestions(questions)
+                }
+            }).then(ans => {
+                return models.TestUserAnswer.findAll({
+                    where: {
+                        idAttempt: idattempt
+                    }
+                }).then(result => {
+                    res.render('../views/tests/result.pug', {
+                        result: result,
+                        ans: ans,
+                        questions: questions
+                    })
+                })
+            })
+        })
+    })
+}
