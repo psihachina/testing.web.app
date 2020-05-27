095e63b22744e6370163a5df30b632165a966108
bogya345 <kkedrovich@gmail.com>
2020-05-27 19:38:39 +0300

    set access control

--- routes/index.js
+++ routes/index.js
@@ -38,26 +38,26 @@ router.post('/subjects/:id', tests.send_test);
 
 //#region  Admin
 
-router.get('/admin', admin.show_adminPage);
+router.get('/admin', hasAuth, admin.show_adminPage);
 
-router.get('/admin/subjects', admin.show_subjects);
-router.post('/admin/subjects/add', admin.subjectAdd);
-router.post('/admin/subjects/delete/:id', admin.subjectDelete);
-router.post('/admin/subjects/edit/:id', admin.subjectEdit);
+router.get('/admin/subjects', hasAuth, admin.show_subjects);
+router.post('/admin/subjects/add', hasAuth, admin.subjectAdd);
+router.post('/admin/subjects/delete/:id', hasAuth, admin.subjectDelete);
+router.post('/admin/subjects/edit/:id', hasAuth, admin.subjectEdit);
 
-router.get('/admin/subjects/:id', admin.show_tests);
-router.post('/admin/tests/add', admin.testAdd);
-router.post('/admin/tests/delete/:id', admin.testDelete);
+router.get('/admin/subjects/:id', hasAuth, admin.show_tests);
+router.post('/admin/tests/add', hasAuth, admin.testAdd);
+router.post('/admin/tests/delete/:id', hasAuth, admin.testDelete);
 router.post('/admin/tests/edit/:id', admin.testEdit);
 
-router.get('/admin/tests/:id', admin.show_questions);
-router.post('/admin/questions/add', admin.questionAdd);
-router.post('/admin/questions/delete/:id', admin.questionDelete);
-router.post('/admin/questions/edit/:id', admin.questionEdit);
+router.get('/admin/tests/:id', hasAuth, admin.show_questions);
+router.post('/admin/questions/add', hasAuth, admin.questionAdd);
+router.post('/admin/questions/delete/:id', hasAuth, admin.questionDelete);
+router.post('/admin/questions/edit/:id', hasAuth, admin.questionEdit);
 
-router.post('/admin/answers/add', admin.answerAdd);
-router.post('/admin/answers/edit/:id', admin.answerEdit);
-router.post('/admin/answers/delete/:id', admin.answerDelete);
+router.post('/admin/answers/add', hasAuth, admin.answerAdd);
+router.post('/admin/answers/edit/:id', hasAuth, admin.answerEdit);
+router.post('/admin/answers/delete/:id', hasAuth, admin.answerDelete);
 
 
 // router.get()
